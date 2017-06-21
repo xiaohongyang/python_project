bdb029a5e14a7ed180a2149272672e1249873144
xiaohongyang <258082291@qq.com>
2017-06-21 22:52:40 +0800

    *

--- test.py
+++ test.py
@@ -1,23 +1,47 @@
-from tkinter   import  Frame, IntVar
+import tkinter as tk
+from tkinter import *
  
 
-class MyCheckButton(Frame) :
-	def __init__(self, mastr = None) :
-		Frame.init( self, master)
-		self.pack(expand = True, fill = BOTH)
+class MyCheckButton(tk.Frame) :
+	def __init__(self, master = None) :
+		super().__init__( master)
+		self.pack(expand = True)
 		self.master.title ("")
 		self.master.minsize(200,200)
-		self.var = IntVar()
+		self.var = tk.IntVar()
 
-		cb = Checkbutton(self, text="Show Title", variable = self.var, command = self.click)
-		cb.place(x = 50, y = 50)
+		cb = tk.Checkbutton(self, text="Show Title", variable = self.var, command = self.click)
+		cb.place(x = 150, y = 150)
+
+		#cb.pack()
+		cb.place(x=50, y=56)
+
+		self.init()
 
 	def click(self):
+		print('clicked')
+		print(self.var.get())
 		if self.var.get() == 1 :
-			self.master.title = "CheckButton"
+			self.master.title("CheckButton")
 		else :
-			self.master.title = ""
+			self.master.title("")
 
+	def init(self):
+
+		l_username = tk.Label(self, text="用户名").grid(row=0)
+		entry_username = Entry(self, bd=1)
+		entry_username.grid(row=0, column=1)
+
+
+		l_password = Label(self, text="密码")
+		l_password.grid(row=1)
+
+		entry_password = Entry(self)
+		entry_password.grid(row=1, column=1)
 
 if __name__ == "__main__" :
-	MyCheckButton().mainloop()
+	root = tk.Tk()
+	app = MyCheckButton(master = root)
+	app.master.title("33")
+
+	app.mainloop()
